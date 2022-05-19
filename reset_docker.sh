a6327f3ade3158e9961b129d3bc59fc83b2df3dc
Kema-dev <01sludge_coaches@icloud.com>
2022-05-19 14:10:54 +0200

    added reset docker.sh

---
+++ reset_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+docker stop $(docker ps -qa)
+docker rm $(docker ps -qa)
+docker rmi -f $(docker images -qa)
+docker volume rm $(docker volume ls -q)
+docker network rm $(docker network ls -q)
