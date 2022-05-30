592c61809dfa9cda30b6bfb3cca96681cba1e723
Kema-dev <--replace-all>
2022-05-30 18:03:06 +0200

    updated gitignore and added debug view

--- website/frontend/vuejs/bind/src/router/index.js
+++ website/frontend/vuejs/bind/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/HomeView.vue"
 import Login from '@/views/LoginView.vue'
+import DebugKemaView from '@/views/DebugKemaView.vue'
 
 const routes = [
 	{
@@ -12,6 +13,11 @@ const routes = [
 		name: 'home',
 		path: '/home',
 		component: Home
+	},
+	{
+		name: 'debug_kema',
+		path: '/debug_kema',
+		component: DebugKemaView
 	},
 ]
 
