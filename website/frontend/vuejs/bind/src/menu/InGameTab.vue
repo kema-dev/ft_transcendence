63d06784545a71790509bd9a25249c85e7f2db5f
Toto Notebook ASUS <thomas.dayde@gmail.com>
2022-07-21 11:19:30 +0200

    boutons in-game not playing

--- website/frontend/vuejs/bind/src/menu/InGameTab.vue
+++ website/frontend/vuejs/bind/src/menu/InGameTab.vue
@@ -1,17 +1,27 @@
 <template>
-	<div>
-
-		<div v-if="playing == false" class="center">
+	<div id="ingame_container">
+		<div v-if="playing == false" class="center column not_playing">
 			<h3>Not actually playing ...</h3>
-			<button></button>
+			<button class="button_ingame" @click="create_room">Create a new room</button>
+			<button class="button_ingame" @click="join_room">Join a room</button>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { inject } from "vue";
 import { useRoute } from 'vue-router'
 
+let define = inject("colors");
 const route = useRoute();
+
+function create_room() {
+	console.log("create a new room");
+}
+
+function join_room() {
+	console.log("Join a room");
+}
 
 let playing = false;
 
@@ -19,5 +29,27 @@ let playing = false;
 </script>
 
 <style>
+
+#ingame_container {
+	--heightContainer: calc(100vh - 180px);
+	height: var(--heightContainer);
+	position: relative;
+}
+
+.not_playing {
+	/* margin-top: calc(0.5 * var(--heightContainer)); */
+	position: absolute;
+	top: 30%;
+}
+
+.button_ingame {
+	background-color: v-bind("define.color2");
+	border-radius: 10px;
+	color: v-bind("define.color0");
+	padding-left: 0.5rem;
+	padding-right: 0.5rem;
+	height: 1.5rem;
+	margin-top: 1rem;
+}
 
 </style>
