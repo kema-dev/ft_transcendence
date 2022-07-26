2fc6c7489bb473c4fff82a4d428bd3933598f377
Thomas Dayde <tdayde@z2r2p2.42lyon.fr>
2022-07-26 15:04:31 +0200

    debut router-link to private/conv

---
+++ website/frontend/vuejs/bind/src/menu/ConversationItem.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h1>{{route.params.conv_name}} route</h1>
+		<h1>{{route.params.conv}} route2</h1>
+		<h1>{{conv?.user?.name}} props</h1>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { inject, defineProps, onMounted, ref } from "vue";
+import { useRoute } from 'vue-router';
+import Conversation from '@/chat/Conversation';
+
+const route = useRoute();
+const props = defineProps({
+  conv: Conversation
+})
+console.log(props.conv);
+</script>
+
+<style scoped>
+
+</style>
