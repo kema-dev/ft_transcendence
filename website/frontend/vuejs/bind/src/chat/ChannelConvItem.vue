2e12f0b42ae3a2251a4a5e8fa82f5a88800e99ee
Thomas Dayde <thomas.dayde@gmail.com>
2022-12-07 00:47:29 +0100

    scroll chat OK

--- website/frontend/vuejs/bind/src/chat/ChannelConvItem.vue
+++ website/frontend/vuejs/bind/src/chat/ChannelConvItem.vue
@@ -192,11 +192,12 @@ function init() {
 			chanRead(index.value, true);
 			let msgsCont = document.getElementById("msgsCont");
 			if (msgsCont) {
-				let oldScrollTop = msgsCont!.scrollTop;
-				let oldScrollHeight = msgsCont!.scrollHeight;
-				let oldClientHeight = msgsCont!.clientHeight;
-				let lastMsg = msgsCont.lastElementChild!.clientHeight;
-				if (oldScrollTop + oldClientHeight + lastMsg == oldScrollHeight)
+				// let oldScrollTop = msgsCont!.scrollTop;
+				// let oldScrollHeight = msgsCont!.scrollHeight;
+				// let oldClientHeight = msgsCont!.clientHeight;
+				// let lastMsg = msgsCont.lastElementChild!.clientHeight;
+				// if (oldScrollTop + oldClientHeight + lastMsg == oldScrollHeight)
+				// 	msgsCont!.scrollTop = msgsCont!.scrollHeight;
 				msgsCont!.scrollTop = msgsCont!.scrollHeight;
 			}
 		}, {flush: 'post'})
